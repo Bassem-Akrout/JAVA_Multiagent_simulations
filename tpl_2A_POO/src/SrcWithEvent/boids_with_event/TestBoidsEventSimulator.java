faa9d84bb53908d73487b40272e93f7bed0c0f6c
Bassem <akroutb@grenoble-inp.org>
2023-11-17 13:01:00 +0100

    modif finales

--- tpl_2A_POO/src/SrcWithEvent/boids_with_event/TestBoidsEventSimulator.java
+++ tpl_2A_POO/src/SrcWithEvent/boids_with_event/TestBoidsEventSimulator.java
@@ -1,13 +1,53 @@
 import gui.GUISimulator;
 import java.awt.Color;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class TestBoidsEventSimulator {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int nbrTypesBoids;
+        int nbrBoids;
+
+        // Demande à l'utilisateur de saisir le nombre de types de boids (supérieur à 1)
+        do {
+            try {
+                System.out.print("Veuillez saisir le nombre de types de boids (supérieur à 1) : ");
+                nbrTypesBoids = scanner.nextInt();
+
+                if (nbrTypesBoids <= 1) {
+                    System.out.println("Veuillez entrer un nombre supérieur à 1.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Veuillez entrer un entier valide.");
+                scanner.next(); // Pour consommer la saisie incorrecte et éviter une boucle infinie
+                nbrTypesBoids = 0; // Réinitialiser la variable pour forcer la répétition de la boucle
+            }
+        } while (nbrTypesBoids <= 1);
+
+        // Demande à l'utilisateur de saisir le nombre de boids (entre 250 et 1000)
+        do {
+            try {
+                System.out.print("Veuillez saisir le nombre de boids (entre 250 et 1000) : ");
+                nbrBoids = scanner.nextInt();
+
+                if (nbrBoids < 250 || nbrBoids > 1000) {
+                    System.out.println("Veuillez entrer un nombre entre 250 et 1000.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Veuillez entrer un entier valide.");
+                scanner.next(); // Pour consommer la saisie incorrecte et éviter une boucle infinie
+                nbrBoids = 0; // Réinitialiser la variable pour forcer la répétition de la boucle
+            }
+        } while (nbrBoids < 250 || nbrBoids > 1000);
+
+        scanner.close();
+
         GUISimulator gui = new GUISimulator(1850, 930, Color.BLACK);
 
-        BoidsEventsSimulator boidsSimulator = new BoidsEventsSimulator(gui, 1000, 1850, 930,3);
+        BoidsEventsSimulator boidsSimulator = new BoidsEventsSimulator(gui, nbrBoids, 1850, 930, nbrTypesBoids);
 
-        //boidsSimulator.restart();
         gui.setSimulable(boidsSimulator);
     }
 }
